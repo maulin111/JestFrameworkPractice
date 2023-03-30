fff44e4911030d2a26c06481563ab3e8e20e398b
Maulin111 <Maulin.desai@einfochips.com>
2023-03-30 18:53:17 +0530

    addedtestcases

---
+++ src/calculator.test.js
@@ -0,0 +1,29 @@
+// importing the javascript function
+const mathOperations = require('./calculator');
+
+describe("Calculator tests", () => {
+    test('adding 1 + 2 should return 3', () => {
+   
+      expect(mathOperations.sum(1, 2)).toBe(3);
+   
+    });
+
+    test('subtract 3 - 2 should return 1', () => {
+   
+      expect(mathOperations.diff(3, 2)).toBe(1);
+   
+    });
+
+    test('Multiply 3 and 2 should return 6', () => {
+   
+      expect(mathOperations.product(3, 2)).toBe(6);
+   
+    });
+
+    test('Multiply 4 and 2 should return 8', () => {
+   
+      expect(mathOperations.product(4, 2)).toBe(8);
+   
+    });
+   
+   })
