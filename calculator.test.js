f54eda5a63f6a30779d7870258fe98f408f68213
Maulin111 <Maulin.desai@einfochips.com>
2023-03-30 17:04:10 +0530

    testproject

---
+++ calculator.test.js
@@ -0,0 +1,10 @@
+const mathOperations = require('./calculator');
+
+describe("Calculator tests", () => {
+    test('adding 1 + 2 should return 3', () => {
+   
+      expect(mathOperations.sum(1, 2)).toBe(3);
+   
+    });
+   
+   })
